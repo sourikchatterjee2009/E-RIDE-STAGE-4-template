bca66b8ae79e49b75395ca8fc9d87478c1b301af
sourikchatterjee2009 <131523481+sourikchatterjee2009@users.noreply.github.com>
2023-11-26 15:44:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
-
-
-
-
+const firebaseConfig = {
+  apiKey: "AIzaSyC2CJFK77q4bVyocW5xBp2ZNMejNTdRa8U",
+  authDomain: "e-library-1a3c3.firebaseapp.com",
+  projectId: "e-library-1a3c3",
+  storageBucket: "e-library-1a3c3.appspot.com",
+  messagingSenderId: "461588273799",
+  appId: "1:461588273799:web:754a59146133520bb23622"
+};
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.firestore();
